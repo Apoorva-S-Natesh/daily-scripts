6d4456d4601186147ac84e2b8d7a5fda628360b0
Apoorva-S-Natesh <apoorva1326@gmail.com>
2024-12-27 05:34:34 +0100

    27th Dec Array methods

--- objects.js
+++ objects.js
@@ -75,3 +75,68 @@ console.log(arr1);
 
 arr1.unshift(3); //Append element at the 0th index
 console.log(arr1);
+
+console.log("Removed element is: ", arr1.pop()); //Removes the last element from array and returns it
+console.log(arr1);
+
+console.log("Removed element by shift", arr1.shift())//Removes element from 0th index (first element)
+console.log(arr1);
+
+////Modifying and accessing elements
+//splice(startIndex, deleteCount, item1,item2 ...)
+//item1,item2 are optional, elements to add into the array at the start position
+
+let colors = new Array("red", "green", "blue", "yellow", "purple", "orange"); // another way of declaring array using Array constructor
+console.log(colors);
+colors.splice(1, 2, "pink", "black"); // [green, blue] replaced with [pink, black]
+console.log(colors);
+
+arr1.push(70, 98, 6);
+console.log(arr1); //[10, 21, 30, 43, 50, 70, 80, 90]
+console.log("deleted elemnts are: ",arr1.splice(3, 2)); //[43, 50] removed could also give what elements should replace these elements
+console.log(arr1); //[10, 21, 30, 70, 80, 90]
+
+/////slice() - returns a ashallow copy of a portion of an array witout modifying the original array. Extracts sub part of array
+console.log(arr1.slice(1,3)); // slice(startIndex, endIndex(exclusive)), emdIndex is not included
+console.log(arr1);
+
+//indexOf() - returns index of specific element
+//In case not present gives -1, in case 2 are present gives the index of the first occurance
+console.log(arr1.indexOf(70));
+
+//// includes() - checks if an element is present or not - returns true or false
+console.log(arr1.includes(300));
+
+/// sort() - sorts elements in place as strings by default
+let letters = ["c", "e", "d", "a", "b"];
+letters.sort();
+console.log(letters);
+arr1.sort();
+console.log(arr1); // Since everything is considered as string it compares the characters like string so doesnt sort numbers
+
+//// reverse() - reverse the order of elements // not reverse sorting just reversing the characters
+letters.reverse();
+console.log(letters);
+arr1.reverse();
+console.log(arr1); 
+
+///////////////////   Array Iteration Methods  ///////////////////////////////////
+
+///concat() - combines two or more arrays and returns a new array
+let arr4 = [100, 200];
+let combined = arr1.concat(arr4);
+console.log(combined);
+
+//// join() - joins all the elements of an array into a string, with or without separator
+let words =["Hell", "O", "WOrld", "!!"];
+let sentence = words.join(""); // HellOWOrld!!
+//let sentence = words.join("-"); // Hell-O-WOrld-!!
+console.log(sentence);
+
+/// Destrcture array /
+// array unpacking - way to unpack values from arrays into distinct variables (separate variables, not array)
+let fruits = ['apple', 'banana', 'grapes', 'guava', 'berrries'];
+let [fruit1, fruit2] = fruits;
+console.log(fruit1); //apple
+console.log(fruit2); //banana
+
