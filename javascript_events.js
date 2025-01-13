e5d959c4a4bf7193ac8843868bb8aa195efc28fb
Apoorva-S-Natesh <apoorva1326@gmail.com>
2025-01-13 12:07:14 +0100

    event lsiteners and some basic use cases

--- javascript_events.js
+++ javascript_events.js
@@ -78,3 +78,38 @@ window.addEventListener('resize', ()=> alert('window resized'));
 
 //scroll - triggered when the user scrolls
 window.addEventListener('scroll', ()=> alert('User is scrolling'));
+
+
+//Event Listener in action
+
+// Select the elements
+const themeContainer = document.getElementById('themeContainer');
+const heading = document.getElementById('heading');
+const themeToggleButton = document.getElementById('themeToggleButton');
+
+// Add event listener for theme toggle
+themeToggleButton.addEventListener('click', () => {
+if (themeContainer.style.backgroundColor === 'black') {
+// Switch to Light Theme
+themeContainer.style.backgroundColor = 'white';
+heading.style.color = 'black';
+themeToggleButton.textContent = 'Switch to Dark Theme';
+} else {
+// Switch to Dark Theme
+themeContainer.style.backgroundColor = 'black';
+heading.style.color = 'white';
+themeToggleButton.textContent = 'Switch to Light Theme';
+}
+});
+
+//Allowing users to dynamically add items to list
+const itemInput = document.getElementById('itemInput');
+const addItemButton = document.getElementById('addItemButton');
+const shoppingList = document.getElementById('shoppingList');
+
+addItemButton.addEventListener('click', () => {
+const newItem = document.createElement('li');
+newItem.textContent = itemInput.value;
+shoppingList.appendChild(newItem);
+itemInput.value = '';
+});
