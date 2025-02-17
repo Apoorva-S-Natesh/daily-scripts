68ccee06b891d7fd9254bc29566a7e30aea1b801
Apoorva-S-Natesh <apoorva1326@gmail.com>
2025-02-17 20:03:44 +0100

    import and  export in javascript

--- javascript/practice_1/app.js
+++ javascript/practice_1/app.js
@@ -1,3 +1,8 @@
 import { apiKey } from "./util.js"; //shloud have used tupe="module" while linking this script in html
+//import apiKey from "./util.js"; -When impoting a default export no need of {} but still needs a name
+//import { apiKey, anotherKey } from "./util.js" // multiple not default imports or 
+//import * as util from "./util.js" // could create a javascript object, everything that util gives is grouped into util.
+//import {apiKey, anotherKey as key} from "./util.js"
 
 console.log(apiKey);
+//console.log(util.apiKey); - when importing multiple things that are grouped using javascript object
--- javascript/practice_1/util.js
+++ javascript/practice_1/util.js
@@ -1,1 +1,5 @@
 export let apiKey = "lfkjsl2341"; //To make it available for other js files
+
+export default "lfkjsl2341"; //only one default export per file, this is the defaultexport from this file. Doesnt need a name
+
+export let anotherKey = "sldkjfe143";
